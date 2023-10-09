eb99778a10fd57b8d1d9cd194190968ae667d6a6
venkat <venkat00248@gmail.com>
2023-10-10 01:07:25 +0530

    Add:routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { MainLayout } from "./components/layouts/Main/MainLayout";
 import "./App.scss";
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
+    <HashRouter>
     <div className="App" data-test-id="app-component">
       <MainLayout />
     </div>
+    </HashRouter>
   );
 }
 
--- frontend/src/components/layouts/Main/MainLayout.tsx
+++ frontend/src/components/layouts/Main/MainLayout.tsx
@@ -1,13 +1,17 @@
 import "./MainLayout.scss";
 import { Header } from "../Header/Header";
+import { useRoutes } from "react-router-dom";
+import { Routes } from "../../routes/Routes";
 export const MainLayout = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const routeResult: any = useRoutes(Routes);
   return (
     <div >
       <Header />
       <div className="main-content">
         <div className="routing">
           <div className="layoutRouting" >
-            Main
+           {routeResult}
           </div>
         </div>
       </div>
